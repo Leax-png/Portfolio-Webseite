eb7b886503880380e8ba55a2601308485cab4520
Leax-png <sebastian.schneider@ish.de>
2025-12-22 17:19:32 +0100

    Enable multi-page build

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-  base: "/Portfolio-Webseite/", // exakt Repo-Name mit Slashes
+  base: "/Portfolio-Webseite/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        architecture: resolve(__dirname, "architecture.html"),
+      },
+    },
+  },
 });
-
